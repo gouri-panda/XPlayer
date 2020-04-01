ac9d4dc491c55faaf3f65bce153e69456283c51a
gouri-panda <gouripanda4@gmail.com>
2020-04-01 14:56:37 +0530

    initial commit

---
+++ app/src/main/java/com/one4ll/xplayer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.one4ll.xplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
