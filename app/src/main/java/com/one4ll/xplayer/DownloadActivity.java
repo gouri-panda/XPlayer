37dc75f7de3bbdcb290da2eb6a4b1106673f394e
gouri-panda <gouripanda4@gmail.com>
2020-04-08 19:07:12 +0530

    downloadActivity created

---
+++ app/src/main/java/com/one4ll/xplayer/DownloadActivity.java
@@ -0,0 +1,52 @@
+package com.one4ll.xplayer;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.strictmode.DiskWriteViolation;
+
+import javax.xml.transform.Result;
+
+public class DownloadActivity extends AppCompatActivity {
+    DownloadManager downloadManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_download);
+        downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        ActivityCompat.requestPermissions(DownloadActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+    }
+
+    private void downloadImageFile() {
+        String url ="https://images.unsplash.com/photo-1586291555476-33e6a30cd7ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=976&q=80";
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        request.setVisibleInDownloadsUi(true);
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE|DownloadManager.Request.NETWORK_WIFI);
+        request.setTitle("xplayer");
+        request.setDescription("description");
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "image");
+        request.allowScanningByMediaScanner();
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        downloadManager.enqueue(request);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            if (requestCode ==0){
+                downloadImageFile();
+            }
+        }
+    }
+}
--- app/src/main/java/com/one4ll/xplayer/MainActivity.java
+++ app/src/main/java/com/one4ll/xplayer/MainActivity.java
@@ -1,22 +1,32 @@
 package com.one4ll.xplayer;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.icu.text.DateFormat;
+import android.icu.text.SimpleDateFormat;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
 import com.google.android.exoplayer2.ExoPlaybackException;
-import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.PlaybackParameters;
 import com.google.android.exoplayer2.Player;
-import com.google.android.exoplayer2.SeekParameters;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.audio.AudioAttributes;
@@ -30,13 +40,17 @@ import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import butterknife.OnItemClick;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    @BindView(R.id.main_activity_constraint_layout)
+    ConstraintLayout constraintLayout;
     @BindView(R.id.player_view)
     PlayerView playerView;
     @BindView(R.id.progressBar)
@@ -45,6 +59,15 @@ public class MainActivity extends AppCompatActivity {
     ImageButton playButton;
     @BindView(R.id.image_button_pause)
     ImageButton pauseButton;
+    @BindView(R.id.image_button_play_2)
+    ImageButton playButton2;
+    @BindView(R.id.image_button_pause_2)
+    ImageButton pauseButton2;
+    @BindView(R.id.imageButtonPrevious)
+    ImageButton imageButtonPrevious;
+    @BindView(R.id.imageButtonNext)
+    ImageButton imageButtonNext;
+
     private SimpleExoPlayer simpleExoPlayer;
 
 
@@ -67,39 +90,79 @@ public class MainActivity extends AppCompatActivity {
         MediaSource mediaSource = new ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(fileUri2);
         simpleExoPlayer.prepare(mediaSource);
         simpleExoPlayer.addAudioListener(audioListener);
-        Log.d(TAG, "onCreate: simple exo player is playing " + simpleExoPlayer.isPlaying());
         simpleExoPlayer.setPlayWhenReady(true);
         simpleExoPlayer.addListener(eventListener);
+        calculateAndGetUriAndFolders();
+         LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver,new IntentFilter("customIntent"));
+//         new TestingLocalBroadCast().execute();
+
     }
+    private  BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG, "onReceive: " + intent.getStringExtra("message"));
+        }
+    };
 
     @OnClick(R.id.image_button_play)
     public void play() {
+        playVideo();
+    }
+
+    @OnClick(R.id.image_button_play_2)
+    public void play2() {
+        playVideo();
+    }
+
+    private void playVideo() {
         Log.d(TAG, "play: clicked");
         if (simpleExoPlayer.getPlaybackState() == Player.STATE_ENDED) {
             simpleExoPlayer.seekTo(0);
         }
         simpleExoPlayer.setPlayWhenReady(true);
         playButton.setVisibility(View.INVISIBLE);
+        playButton2.setVisibility(View.INVISIBLE);
+        pauseButton2.setVisibility(View.VISIBLE);
+
     }
+
 
     @OnClick(R.id.player_view)
     public void onPlayViewClicked() {
         pauseButton.setVisibility(View.VISIBLE);
+        pauseButton2.setVisibility(View.VISIBLE);
     }
 
     @OnClick(R.id.image_button_pause)
     public void pause() {
+        pauseVideo();
+    }
+
+    @OnClick(R.id.image_button_pause_2)
+    public void pause2() {
+        pauseVideo();
+    }
+
+    @OnClick(R.id.imageButtonPrevious)
+    public void goToPrevious() {
+        int state = simpleExoPlayer.getPlaybackState();
+    }
+
+    private void pauseVideo() {
         progressBar.setVisibility(View.GONE);
         simpleExoPlayer.setPlayWhenReady(false);
         pauseButton.setVisibility(View.INVISIBLE);
+        pauseButton2.setVisibility(View.INVISIBLE);
         playButton.setVisibility(View.VISIBLE);
+        playButton2.setVisibility(View.VISIBLE);
+
     }
 
 
     private Player.EventListener eventListener = new Player.EventListener() {
         @Override
         public void onTimelineChanged(Timeline timeline, int reason) {
-            Log.d(TAG, "onTimelineChanged: timeline " + timeline);
+            Log.d(TAG, "onTimelineChanged: timeline " + timeline.toString());
             Log.d(TAG, "onTimelineChanged: reason " + reason);
         }
 
@@ -184,6 +247,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
         }
 
         @Override
@@ -217,5 +281,92 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         simpleExoPlayer.release();
         super.onDestroy();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiver);
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void setTime(long timeInMills) {
+        String timeString;
+        int hour = -1;
+        int minutes = -1;
+        int secs = -1;
+        hour = (int) TimeUnit.MILLISECONDS.toHours(timeInMills);
+        minutes = (int) TimeUnit.MILLISECONDS.toMinutes(timeInMills);
+        secs = (int) TimeUnit.MILLISECONDS.toSeconds(timeInMills);
+
+
+        Log.d(TAG, "pauseVideo: duration " + timeInMills);
+        Log.d(TAG, "onCreate: hour " + hour);
+        Log.d(TAG, "onCreate: minute " + minutes);
+        Log.d(TAG, "onCreate: time" + secs);
+        if (hour != 0 && minutes != 0 && secs != 0) {
+            String stringHour = String.valueOf(hour);
+            String stringMinutes = String.valueOf(minutes);
+            String stringSeconds = String.valueOf(secs);
+            timeString = stringHour + stringMinutes + stringSeconds;
+            Log.d(TAG, "setTime: " + timeString);
+
+        } else if (minutes != 0 && secs != 0) {
+            String stringMinutes = String.valueOf(minutes);
+            String stringSeconds = String.valueOf(secs);
+            timeString = stringMinutes + stringSeconds;
+            Log.d(TAG, "setTime: " + timeString);
+
+        } else {
+            String stringSeconds = String.valueOf(secs);
+            timeString = stringSeconds;
+            Log.d(TAG, "setTime: " + timeString);
+
+        }
+        DateFormat dateFormat = new SimpleDateFormat("HH 'hours' ,mm 'mins',ss 'secs' ");
+        Log.d(TAG, "setTime: date format" + dateFormat.format(new Date(timeInMills)));
+    }
+
+    private void calculateAndGetUriAndFolders() {
+        String[] projection = {MediaStore.Video.Media._ID, MediaStore.Video.Media.ALBUM, MediaStore.Video.Media.DATE_ADDED, MediaStore.Video.Media.CONTENT_TYPE};
+        Cursor cursor = getContentResolver().query(MediaStore.Images.Media.INTERNAL_CONTENT_URI,null,null,null,null );
+        if (cursor == null){
+            Log.d(TAG, "calculateAndGetUriAndFolders: cursror is null");
+        }
+        while (cursor != null && cursor.moveToFirst()){
+           String  stringUri = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME.toString()));
+            Log.d(TAG, "calculateAndGetUriAndFolders: string uri" + stringUri);
+        }
+        if (cursor != null){
+            cursor.close();
+        }
+
+    }
+
+
+    //Stupid timing set
+//    private void setTime(long timeInMils){
+//        int hour  = -1;
+//        int minute = -1;
+//        int sec = -1;
+//
+//        if (timeInMils > 3600000){
+//            hour =(int) timeInMils / 3600000;
+//            minute =(int) timeInMils / 360000;
+//            sec  =(int) timeInMils /1000;
+//        }else if (timeInMils < 3600000 && timeInMils > 360000){
+//            minute =(int) (timeInMils / 1000 ) / 60;
+
+//            sec  =(int) (timeInMils /360000) % 60;
+//        }else {
+//            sec  =(int) (timeInMils /360000) % 60;
+//        }
+//        if (hour != -1){
+//            if (minute != -1){
+//                if (sec != -1){
+//                    Log.d(TAG, "setTime: hour" + hour +"minute"+ minute + "sec" + sec );
+//                }
+//            }
+//        }else if (minute != -1 && sec != -1){
+//            Log.d(TAG, "setTime: minute" + minute + "sec " + sec);
+//        }else{
+//            Log.d(TAG, "setTime: sec" + sec);
+//        }
+//    }
+
 }
