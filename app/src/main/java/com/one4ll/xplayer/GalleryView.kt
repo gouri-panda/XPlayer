43dc4cd4a164b7084e3691717b188f09e3c4a267
gouri-panda <gouripanda4@gmail.com>
2020-07-10 16:01:44 +0530

    added theme and color

--- app/src/main/java/com/one4ll/xplayer/GalleryView.kt
+++ app/src/main/java/com/one4ll/xplayer/GalleryView.kt
@@ -1,6 +1,9 @@
 package com.one4ll.xplayer
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -12,22 +15,30 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
-
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+private val TAG = "galleryview"
 class GalleryView : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var navController : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery_view)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-
-
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        sharedPreferences = getSharedPreferences("galleryView", Context.MODE_PRIVATE)
+        val destination = sharedPreferences.getInt("lastDestination",R.id.nav_home)
+        val navOptions = NavOptions.Builder().setPopUpTo(destination,true).build()
         val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
+         navController = findNavController(R.id.nav_host_fragment)
+        navController.navigate(R.id.mobile_navigation,null,navOptions)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
@@ -45,5 +56,17 @@ class GalleryView : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onDestroy() {
+        val  lastDestinationId = navController.currentDestination?.id
+        if (lastDestinationId != null){
+            sharedPreferences.edit().putInt("lastDestination",lastDestinationId).apply()
+            Log.d(TAG, "onDestroy: lastDestinationid $lastDestinationId")
+        }
+        super.onDestroy()
+
+
+
     }
 }
---
+++ app/src/main/java/com/one4ll/xplayer/ui/stream/StreamViewModel.kt
@@ -0,0 +1,6 @@
+package com.one4ll.xplayer.ui.stream
+
+import androidx.lifecycle.ViewModel
+
+class StreamViewModel : ViewModel() {
+}
