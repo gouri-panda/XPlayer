c5f623bd8d14ce30bfc028fce359f0a708a23523
gouri-panda <gouripanda4@gmail.com>
2020-07-14 22:15:33 +0530

    fixed  bug of StreamFragment.kt

--- app/src/main/java/com/one4ll/xplayer/ui/stream/StreamFragment.kt
+++ app/src/main/java/com/one4ll/xplayer/ui/stream/StreamFragment.kt
@@ -29,48 +29,21 @@ class StreamFragment : Fragment() {
     private var param2: String? = null
     private lateinit var rootView: View
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_stream, container, false)
-        rootView?.url_send.setOnClickListener {
-            val url = rootView?.editTextUrl?.text
+        rootView.url_send.setOnClickListener {
+            val url = rootView.editTextUrl?.text
             if (url != null) {
                 val intent = Intent(rootView.context, MainActivity::class.java)
-                intent.putExtra("video_path", url)
+                intent.putExtra(VIDEO_PATH, url.toString())
                 Log.d(TAG, "onCreateView: stream url path $url")
                 rootView.context.startActivity(intent)
 
             }
         }
         return rootView
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StreamFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                StreamFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
     }
 }
