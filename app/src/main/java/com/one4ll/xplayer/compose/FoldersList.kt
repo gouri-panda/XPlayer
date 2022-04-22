ed68c9f82cd2b77674c6392136f9047ef2ed69da
Gouri Panda <gouripanda4@gmail.com>
2022-04-22 11:23:15 +0530

    FoldersList icon and it's size updated

--- app/src/main/java/com/one4ll/xplayer/compose/FoldersList.kt
+++ app/src/main/java/com/one4ll/xplayer/compose/FoldersList.kt
@@ -2,16 +2,12 @@ package com.one4ll.xplayer.compose
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,12 +15,12 @@ import androidx.compose.ui.unit.dp
 import com.one4ll.xplayer.R
 
 @Composable
-fun FolderList(
+fun FoldersList(
     modifier: Modifier = Modifier,
     folders: List<String>,
     onClick: (String) -> Unit
 ) {
-    Surface(color = colorResource(id = R.color.white)) {
+    Surface {
         LazyColumn(modifier = modifier) {
             for (folder in folders) {
                 item {
@@ -38,8 +34,9 @@ fun FolderList(
                                     }
                             ) {
                                 Image(
-                                    painter = painterResource(id = R.drawable.exo_icon_circular_play),
+                                    painter = painterResource(id = R.drawable.ic_baseline_folder_24),
                                     contentDescription = "",
+                                    modifier = Modifier.size(width = 24.dp, height = 24.dp)
                                 )
                             }
                             Column {
@@ -72,6 +69,10 @@ fun FolderList(
 @Preview
 @Composable
 fun Preview_FolderList() {
-    val fakeFolderList = listOf("one", "two", "three", "four", "five")
-    FolderList(folders = fakeFolderList, onClick = {})
+    val fakeFolderList = listOf("Camera", "Download", "Internal memory", "New folder", "Recordings")
+    FoldersList(
+        folders = fakeFolderList,
+        onClick = {},
+        modifier = Modifier
+    )
 }
